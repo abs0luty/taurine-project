93d048c5f540a963079179a019bff6ac27ce1ff8
Adi Salimgereev <adisalimgereev@gmail.com>
2024-01-02 18:42:14 +0600

    Add tau tests for arty

--- arty/tests.c
+++ arty/tests.c
@@ -1,22 +1,41 @@
 #include "utf8.h"
 #include "unicode.h"
-#include <stdio.h>
-#include <assert.h>
-#include <string.h>
+#include "../tau/tau.h"
 
-int main()
+TEST(arty_tests, arty_utf8_bytes_in_codepoint)
 {
-    assert(arty_utf8_bytes_in_codepoint(0x1D11E) == 4);
-    assert(!arty_is_valid_unicode_codepoint(0x110000));
-
-    arty_codepoint_t codepoint = arty_decode_codepoint_from_utf8("П");
-    arty_codepoint_size_t size = arty_utf8_bytes_in_codepoint(codepoint);
-    assert(codepoint == 0x041F);
-    assert(size == 2);
-
-    char dst[3];
-    arty_encode_codepoint_in_utf8_to_null_terminated_string(codepoint, dst);
-    assert(strcmp(dst, "П") == 0);
-
-    return 0;
+  CHECK_EQ(arty_utf8_bytes_in_codepoint(0x1D11E), 4);
+  CHECK_EQ(arty_utf8_bytes_in_codepoint(0x110000), 0);
+  CHECK_EQ(arty_utf8_bytes_in_codepoint(0x39), 1);
+  CHECK_EQ(arty_utf8_bytes_in_codepoint(0x041F), 2);
 }
+
+TEST(arty_tests, arty_is_valid_unicode_codepoint) {
+  CHECK(!arty_is_valid_unicode_codepoint(0x110000));
+  CHECK(arty_is_valid_unicode_codepoint(0x041F));
+}
+
+TEST(arty_tests, arty_decode_codepoint_from_utf8) {
+  arty_codepoint_t codepoint = arty_decode_codepoint_from_utf8("П");
+  CHECK_EQ(codepoint, 0x041F);
+
+  codepoint = arty_decode_codepoint_from_utf8("p");
+  CHECK_EQ(codepoint, 0x0070);
+}
+
+TEST(arty_tests, arty_utf8_bytes_in_codepoint_by_leading_byte) {
+  const char* cyrillic_p = "П";
+  const char* p = "p";
+
+  CHECK_EQ(arty_utf8_bytes_in_codepoint_by_leading_byte(cyrillic_p[0]), 2);
+  CHECK_EQ(arty_utf8_bytes_in_codepoint_by_leading_byte(p[0]), 1);
+}
+
+TEST(arty_tests, arty_encode_codepoint_in_utf8_to_null_terminated_string) {
+  char dst[3];
+  arty_codepoint_t codepoint = 0x041F;
+  arty_encode_codepoint_in_utf8_to_null_terminated_string(codepoint, dst);
+  CHECK_STREQ(dst, "П");
+}
+
+TAU_MAIN()
